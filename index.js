9daeabed0bae38a863fa8dc3b840ea2b260e76a4
Bry Cox <carl.cc@gmail.com>
2019-11-27 18:52:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+
+function createEmployeeRecord(employeeArr) {
+  return {
+    firstName: employeeArr[0],
+    familyName: employeeArr[1],
+    title: employeeArr[2],
+    payPerHour: employeeArr[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+};
+
+function createEmployeeRecords(employeeArrays){
+    return employeeArrays.map( employeeArr => {
+        return createEmployeeRecord(employeeArr)
+    });
+}
+
+function createTimeInEvent(employee, dateStamp){
+    employee.timeInEvents.push(createEvent("TimeIn", dateStamp))
+    return employee
+}
+
+function createEvent(type, dateStamp){
+
+    const [year, month, day, hour] = dateStamp.split(/[\s-]/g)
+
+    const event = {
+        type: type,
+        hour: parseInt(hour),
+        date: `${year}-${month}-${day}`
+    }
+
+    return event
+}
